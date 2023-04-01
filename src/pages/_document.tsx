22ba3fecdcc07b01f055261a0df2c2d30d47b607
ArthurMaciel95 <arthurnmrocha@gmail.com>
2023-04-01 19:39:47 -0300

    feat(): seo imagem

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -6,12 +6,19 @@ class MyDocument extends Document {
     return (
       <Html>
         <Head>
+          <title>CrisCell - Aparelho de curto em placas eletrônicas</title>
+          <meta
+            name="description"
+            content="Detecta curto em qualquer placa eletrônica. Celulares, TVs, aparelhos de Som, porteiro eletrônico, placas inverter, enfim, qualquer placa de circuitos eletrônicos."
+          />{' '}
+          <meta property="og:image" content="/seo/seo_default.png" />
+          <meta property="og:type" content="website" />
           <link rel="preconnect" href="https://fonts.googleapis.com" />
           <link rel="preconnect" href="https://fonts.gstatic.com" />
           <link rel="manifest" href="/manifest.json" />
           <link rel="icon" type="image/x-icon" href="/favicon.svg"></link>
           <link rel="apple-touch-icon" href="/icon.png"></link>
-          <meta name="theme-color" content="#fff" />
+          <meta name="theme-color" content="#35B6FF" />
           <link
             href="https://fonts.googleapis.com/css2?family=Roboto:ital,wght@0,100;0,300;0,400;0,500;0,700;0,900;1,100;1,300;1,400;1,500;1,700;1,900&display=swap"
             rel="stylesheet"
